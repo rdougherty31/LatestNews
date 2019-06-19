9d21246c84dcd284457acc73f90a341bf58bc609
Rachael <rdougherty031@gmail.com>
2019-06-19 12:42:58 -0400

    updated delete route

--- models/Article.js
+++ models/Article.js
@@ -1,10 +1,9 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
+// Reference to Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
+// Create an ArticleSchema object
 var ArticleSchema = new Schema({
   // `title` is required and of type String
   title: {
@@ -16,17 +15,14 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
   }
 });
 
-// This creates our model from the above schema, using mongoose's model method
+// Store Article model in variable
 var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Article model
+// Export the Article model variable
 module.exports = Article;
--- public/app.js
+++ public/app.js
@@ -1,106 +1,66 @@
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-  }
-});
-
-
-// Whenever someone clicks a p tag
-$(document).on("click", ".deleteNote", function() {
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-  console.log("This ID is: "+thisId);
-//   // Empty the notes from the note section
-//   $(".notes p").empty();
-
-  // Now make an ajax call for the Article
-  $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
-  })
-    // With that done, add the note information to the page
-    .then(function(data) {
-      console.log(data);
-    //   // The title of the article
-    //   $("#notes").append("<h2>" + data.title + "</h2>");
-    //   // An input to enter a new title
-    //   $("#notes").append("<input id='titleinput' name='title' >");
-    //   // A textarea to add a new note body
-    //   $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-    //   // A button to submit a new note, with the id of the article saved to it
-    //   $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-        $(".addNote").attr("data-id",thisId).css("display","none");
-        $(".deleteNote").attr("data-id",thisId).css("display","initial");
-      } else {
-        $(".addNote").attr("data-id",thisId).css("display","initial");
-        $(".deleteNote").attr("data-id",thisId).css("display","none");
-      }
-    });
-});
-// When you click the addnote button on a specific article
-function addNote() {
+// Delete Note Button On Click
+$(document).on("click", ".deleteNote", function () {
+    // Save the note's id which is connected to the delete button
     var thisId = $(this).attr("data-id");
-    $("#addNoteBtn").attr("data-id", thisId);
-    $(".notesOverlay").css("display","none");
-    $("#addNoteWrapper").css("visibility","inherit");
-    console.log(".addNote function");
-}
-// When you click the savenote button
-$(document).on("click", "#addNoteBtn", function() {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "POST",
-    url: "/articles/" + thisId,
-    data: {
-    //   // Value taken from title input
-    //   title: $("#titleinput").val(),
-      // Value taken from note textarea
-      note: $("#bodyinput").val()
-    }
-  })
-    // With that done
-    .then(function(data) {
-      // Log the response
-      console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
+    console.log("This ID is: " + thisId);
+    
+    $.ajax({
+        method: "DELETE",
+        url: "/articles/delete/" + thisId,
+        data: note
+    }).then(function (data) {
+        console.log(data);
+        console.log("note deleted");
+        $("p").attr("data-id", thisId).html("");
     });
-
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
-  $("#addNoteWrapper").css("visibility","hidden");
-  $(".notesOverlay").css("display","block");
-  $(".notesBtn").attr("data-state","1");
 });
 
-// $(".notesBtn").click(function() {
-//     event.preventDefault();
-//     $(".notesOverlay").css("display","block");
-// });
+// Addnote button on click for a specific article
+$(document).on("click", ".addNote", function () {
+    var thisId = $(this).attr("data-id");
+    console.log(thisId);
+    $("#addNoteBtn").attr("data-id", thisId);
+    $(".notesOverlay").css("display", "none");
+    $("#addNoteWrapper").css("visibility", "inherit");
+    console.log(".addNote function");
+});
+// Savenote button on click
+$(document).on("click", "#addNoteBtn", function () {
+    var thisId = $(this).attr("data-id");
+    var newNote = $("#bodyinput").val();
+    console.log(thisId);
+    console.log($("#bodyinput").val());
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+            note: newNote
+        }
+    })
+        .then(function (data) {
+            //Log response & empty notes field
+            console.log(data);
+            $("#notes").empty();
+        });
+
+    // empty input values & hide add note field
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+    $("#addNoteWrapper").css("visibility", "hidden");
+    $(".notesOverlay").css("display", "block");
+    $(".notesBtn").attr("data-state", "1");
+});
 
 function showNotes() {
     var dataState = $(".notesBtn").attr("data-state");
     if (dataState === "0") {
-        $(".notesOverlay").css("display","block");
-        $(".notesBtn").attr("data-state","1");
+        $(".notesOverlay").css("display", "block");
+        $(".notesBtn").attr("data-state", "1");
         $(".notesBtn").text("Hide Notes");
     } else {
-        $(".notesOverlay").css("display","none");
-        $(".notesBtn").attr("data-state","0");
+        $(".notesOverlay").css("display", "none");
+        $(".notesBtn").attr("data-state", "0");
         $(".notesBtn").text("Show Notes");
     }
 }
